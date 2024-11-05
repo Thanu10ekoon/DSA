027b404b38e3c0381f80a824c55caf7975093d48
Thanujaya Tennekoon <eg245364@engug.ruh.ac.lk>
2024-11-05 14:35:03 +0530

    Day 04 of DSA

---
+++ Lectures/Day 04/LinkedList01/LinkedList01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a new linked list of strings
+        LinkedList<int> list = new LinkedList<int>();
+
+        // Add elements to the linked list
+        list.AddLast(1);
+        list.AddLast(2);
+        list.AddLast(3);
+
+        // Display the elements in the linked list
+        Console.WriteLine("Linked List elements:");
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Add an element at the start
+        list.AddFirst(0);
+        Console.WriteLine("\nAfter adding 'Zeroth' at the start:");
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Remove an element
+        list.Remove(2);
+        Console.WriteLine("\nAfter removing 'Second':");
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ Lectures/Day 04/LinkedList02/LinkedList02/Program.cs
@@ -0,0 +1,13 @@
+﻿using LinkedList02;
+
+SLinkedList list = new SLinkedList();
+
+list.AddFront(10);
+list.AddFront(20);
+list.AddFront(30);
+
+list.Print();
+
+list.InsertAt(2,100);
+Console.WriteLine("........");
+list.Print();
